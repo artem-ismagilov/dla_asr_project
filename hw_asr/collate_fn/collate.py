41ef9b686cd2f9769e26a4c61f29c2ea401af510
Artem Ismagilov <a.ismagilow@gmail.com>
2022-10-16 18:33:36 +0300

    Handle error

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -11,23 +11,25 @@ def collate_fn(dataset_items: List[dict]):
     """
 
     result_batch = dict()
+    try:
+        for k in ['spectrogram']:
+            result_batch[k] = torch.nn.utils.rnn.pad_sequence(
+                [torch.squeeze(s[k]).T for s in dataset_items],
+                batch_first=True,
+            ).permute(0, 2, 1,)
 
-    for k in ['spectrogram']:
-        result_batch[k] = torch.nn.utils.rnn.pad_sequence(
-            [torch.squeeze(s[k]).T for s in dataset_items],
-            batch_first=True,
-        ).permute(0, 2, 1,)
+        for k in ['audio', 'text_encoded']:
+            result_batch[k] = torch.nn.utils.rnn.pad_sequence(
+                [torch.squeeze(s[k]) for s in dataset_items],
+                batch_first=True,
+            )
 
-    for k in ['audio', 'text_encoded']:
-        result_batch[k] = torch.nn.utils.rnn.pad_sequence(
-            [torch.squeeze(s[k]) for s in dataset_items],
-            batch_first=True,
-        )
+        for k in ['text', 'audio_path']:
+            result_batch[k] = [s[k] for s in dataset_items]
 
-    for k in ['text', 'audio_path']:
-        result_batch[k] = [s[k] for s in dataset_items]
-
-    result_batch['text_encoded_length'] = torch.tensor([s['text_encoded'].shape[1] for s in dataset_items])
-    result_batch['spectrogram_length'] = torch.tensor([s['spectrogram'].shape[2] for s in dataset_items])
+        result_batch['text_encoded_length'] = torch.tensor([s['text_encoded'].shape[1] for s in dataset_items])
+        result_batch['spectrogram_length'] = torch.tensor([s['spectrogram'].shape[2] for s in dataset_items])
+    except:
+        print('Error in collate function!',)
 
     return result_batch
